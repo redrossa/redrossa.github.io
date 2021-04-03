e845c6fc05be1388120e9c041d0f00bfe8db87f9
Reno Raksi <adriano.raksi@gmail.com>
2021-04-02 20:03:53 -0500

    Deployable on Github

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from './pages/About.js';
 import Navbar from './components/Navbar';
 import NavItem from "./components/NavItem";
 
-import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 export default function App() {
   return (
