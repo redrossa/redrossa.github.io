e33d15fcf5ad416615f6cd57b5c39d83cc1453f8
Reno Raksi <adriano.raksi@gmail.com>
2021-04-03 01:42:50 -0500

    Edit work in progress tools

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,15 +17,15 @@ export default function Home() {
       <div className='Home'>
         <h1>Redrossa's Toolkit</h1>
         <Toolkit>
-          <Tool dest='/' icon={StatsLogo}>Statistics Calculator</Tool>
-          <Tool dest='/' icon={MatrixLogo}>Linear Algebra Step-by-Step Evaluator</Tool>
-          <Tool dest='/' icon={SetsLogo}>Sets Evaluator</Tool>
-          <Tool dest='/' icon={TruthTableLogo}>Predicate Truths Tabulator</Tool>
+          <Tool dest='/' icon={StatsLogo}>Statistics Calculator 🔨</Tool>
+          <Tool dest='/' icon={MatrixLogo}>Linear Algebra Step-by-Step Evaluator 🔨</Tool>
+          <Tool dest='/' icon={SetsLogo}>Sets Evaluator 🔨</Tool>
+          <Tool dest='/' icon={TruthTableLogo}>Predicate Truths Tabulator 🔨</Tool>
           <Tool href dest='http://lc3tutor.org' icon={LC3Logo}>LC-3 Simulator by William Chargin</Tool>
           <Tool href dest='https://regexr.com' icon={RegexLogo}>Regex Builder by gskinner</Tool>
-          <Tool dest='/' icon={RadixSortLogo}>Radix Sort Visualizer</Tool>
-          <Tool dest='/' icon={RBTLogo}>Red-Black Tree Visualizer</Tool>
-          <Tool dest='/' icon={DijkstraLogo}>Dijkstra's Shortest Path Visualizer</Tool>
+          <Tool dest='/' icon={RadixSortLogo}>Radix Sort Visualizer 🔨</Tool>
+          <Tool dest='/' icon={RBTLogo}>Red-Black Tree Visualizer 🔨</Tool>
+          <Tool dest='/' icon={DijkstraLogo}>Dijkstra's Shortest Path Visualizer 🔨</Tool>
         </Toolkit>
       </div>
   );
